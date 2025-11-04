f50e07e0fca4fad5fbb2d9e1ea0f7a00d167babf
dohyeon <qkrehgus02@naver.com>
2025-11-04 15:05:13 +0900

    초기세팅

---
+++ src/main/java/com/saeal/MrDaebackService/MrDaebackServiceApplication.java
@@ -0,0 +1,13 @@
+package com.saeal.MrDaebackService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrDaebackServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrDaebackServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saeal/MrDaebackService/MrDaebackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saeal.MrDaebackService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrDaebackServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
